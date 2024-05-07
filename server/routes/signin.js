7e003ee2acb28800647a1a5aa5643094a0cf323a
Gabriel Estuardo García Donis <gar21352@uvg.edu.gt>
2024-05-06 18:46:57 -0600

    implement is_creator
    Para utilizar is_creator, es necesario colocarlo en la ruta

--- server/routes/signin.js
+++ server/routes/signin.js
@@ -4,7 +4,8 @@ const signinSchema = new mongoose.Schema({
   user_id: Number,
   name: String,
   username: String,
-  password: String
+  password: String,
+  is_creator: Boolean,
 });
 
 const signin = mongoose.model("Usuarios", signinSchema, "Usuarios");
