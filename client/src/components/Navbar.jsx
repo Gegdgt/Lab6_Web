f677385e2498844d3a0893334ca4a4420987bc65
Gabriel Estuardo García Donis <gar21352@uvg.edu.gt>
2024-05-06 18:36:38 -0600

    update navbar
    Se actualizo el navbar para que este cambien conforme los permisos que tiene el usuario, si no esta logueado no permite meterse a otras funciones

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,44 @@
 import React, { useState } from 'react';
-import './Navbar.css'; // Importa el archivo CSS para estilos
+import { useNavigate } from 'react-router-dom'; // Asegúrate de importar useNavigate
+import './Navbar.css';
+import { useUser } from '../context/UserContext';
 
 function Navbar() {
   const [searchTerm, setSearchTerm] = useState('');
+  const { username, is_creator, logout } = useUser();
+  const navigate = useNavigate(); // Define el useNavigate
+
+  const handleLogout = () => {
+    localStorage.removeItem('username');
+    localStorage.removeItem('is_creator'); // Borra el is_creator
+    logout(); 
+    navigate('/login');
+  };
+
   return (
     <div className="navbar">
       <div className="navbar-brand"><a href="/">MyTube</a></div>
       <div className="navbar-links">
-        <a href="/user">Usuarios</a>
-        <a href="Create">Crear</a>
-        <a href="/search">Buscador</a>
+        {username && is_creator ? (
+          <>
+            <a href="/user">{username}</a>
+            <a href="/create">Crear</a>
+            <a href="/search">Buscador</a>
+            <a href="/login" onClick={handleLogout}>Cerrar Sesión</a>
+          </>
+        ) : username ? (
+          <>
+            <a href="/user">{username}</a>
+            <a href="/terms">Términos</a>
+            <a href="/search">Buscador</a>
+            <a href="/login" onClick={handleLogout}>Cerrar Sesión</a>
+          </>
+        ) : (
+          <>
+            <a href="/login">Iniciar Sesión</a>
+            <a href="/signin">Registrarse</a>
+          </>
+        )}
       </div>
     </div>
   );
